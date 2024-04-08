8377ff48298056f34cd4593ee1a3af74785264e7
unknown <ali.jalilvand2203@gmail.com>
2024-04-08 14:48:07 +0330

    first commit

---
+++ src/Voting.java
@@ -0,0 +1,9 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+public class Voting {
+    private int type;
+    private String question;
+    private HashMap<String, HashSet<Vote>> choices;
+
+}
